9a32a0d0f04f5010cc7b44511e0a58dc918de223
narayan1201 <narayan16@gmail.com>
2021-01-08 22:23:21 +0530

    perception launch

--- sahayak_bot/my_object_recognition_pkg/scripts/grasp_demo.py
+++ sahayak_bot/my_object_recognition_pkg/scripts/grasp_demo.py
@@ -4,6 +4,10 @@ import rospy
 import moveit_commander
 import geometry_msgs.msg
 from tf import TransformListener
+from object_msgs.msg import ObjectPose
+
+
+#var_handle_pub = rospy.Publisher('/detection_info', ObjectPose, queue_size=10)
 
 moveit_commander.roscpp_initialize(sys.argv)
 rospy.init_node('grasp_demo', anonymous=True)
@@ -15,25 +19,28 @@ arm_group = moveit_commander.MoveGroupCommander("arm_planning_group")
 hand_group = moveit_commander.MoveGroupCommander("grip_planning_group")
 # Put the grip in the start position
 
-arm_group.set_named_target("object6")
+arm_group.set_named_target("object3")
 plan1 = arm_group.go()
 
 
 t = TransformListener()
 
-t.waitForTransform("/odom", "/object_102", rospy.Time(), rospy.Duration(4.0))
+t.waitForTransform("/shoulder_link", "/object_102", rospy.Time(), rospy.Duration(4.0))
 #if t.frameExists("odom") and t.frameExists("object_101"):
-(translation,rotation) = t.lookupTransform("/odom", "/object_102", rospy.Time())
+(translation,rotation) = t.lookupTransform("/shoulder_link", "/object_102", rospy.Time())
 
-
+#var_handle_pub.publish('glue','translation')
 pose_target = geometry_msgs.msg.Pose()
 pose_target.orientation.w = 0.0
-pose_target.orientation.x = 0.0
-pose_target.orientation.y = 0.0
+pose_target.orientation.x = -0.0
+pose_target.orientation.y = 1.0
 pose_target.orientation.z = 0.0
-pose_target.position = translation 
-pose_target.position.z = 1.0
+#pose_target.position = translation 
+pose_target.position.x = translation[0]
+pose_target.position.y = translation[1]-0.15
+pose_target.position.z = 0.94446979642 
 arm_group.set_pose_target(pose_target)
+
 plan4 = arm_group.go()
 
 
