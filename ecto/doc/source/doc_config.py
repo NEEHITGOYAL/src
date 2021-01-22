c25399f7c24577ad2cc2f4f5e3ca41d10f701de1
dark00infinity <deepaksingh20899@gmail.com>
2021-01-22 08:29:37 -0800

    hello world

--- ecto/doc/source/doc_config.py
+++ ecto/doc/source/doc_config.py
@@ -1,15 +1,15 @@
 version=''
-commithash='c34d9d78b633311d31525fdf80190b37714b3948'
+commithash='960e79fa2c8afbbe8e03d5f511444f160cb68264'
 gittag_short=''
-gittag_long='c34d9d7-dirty'
-git_lastmod='Thu, 14 Jan 2021 22:34:50 +0530'
+gittag_long='960e79f'
+git_lastmod='Sat, 16 Jan 2021 23:19:59 +0530'
 github_url='https://github.com/plasmodic/ecto'
 
 breathe_default_project = 'ecto'
-breathe_projects = dict(ecto='/home/neehit/catkin_ws/build/ecto/doc/../api/xml')
+breathe_projects = dict(ecto='/home/dark/catkin_ws/build/ecto/doc/../api/xml')
 
 # for debug: this is only if you build everything locally
-#ecto_module_url_root = '/home/neehit/catkin_ws/build/ecto/doc/../../doc/html/'
+#ecto_module_url_root = '/home/dark/catkin_ws/build/ecto/doc/../../doc/html/'
 # for release
 ecto_module_url_root = 'http://plasmodic.github.com/'
 
