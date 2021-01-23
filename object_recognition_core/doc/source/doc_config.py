1e80f76c8726e3abe7dc3b2aba02a83fba2647f4
neehit <goyalneehit@gmail.com>
2021-01-23 21:55:37 +0530

    added task 5 package

--- object_recognition_core/doc/source/doc_config.py
+++ object_recognition_core/doc/source/doc_config.py
@@ -6,7 +6,7 @@ git_lastmod=''
 github_url=''
 
 breathe_default_project = 'object_recognition_core'
-breathe_projects = dict(object_recognition_core='/home/dark/catkin_ws/build/object_recognition_core/doc/../api/xml')
+breathe_projects = dict(object_recognition_core='/home/neehit/catkin_ws/build/object_recognition_core/doc/../api/xml')
 
 # for release
 ork_module_url_root = 'http://wg-perception.github.com/'
