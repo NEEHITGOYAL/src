38dc37fb1978dddf2f8bcde25e474b7500fe51d0
neehit <goyalneehit@gmail.com>
2021-01-24 13:40:49 +0530

    ebot_ur5 removed octomap and added pose

--- sahayak_bot/ebot_task5_ur5/scripts/dropbox2_task5.py
+++ sahayak_bot/ebot_task5_ur5/scripts/dropbox2_task5.py
@@ -14,13 +14,13 @@ moveit_commander.roscpp_initialize(sys.argv)
 robot = moveit_commander.RobotCommander()
 hand_group = moveit_commander.MoveGroupCommander("grip_planning_group")
 arm_group = moveit_commander.MoveGroupCommander("arm_planning_group")
-#arm_group.set_named_target("travel")
-#plan1 = arm_group.go()
+arm_group.set_named_target("travel2")
+plan1 = arm_group.go()
 
 def bot_driver():
 
     # Initializes the ROS node for the process.
-    rospy.init_node('nav_test', anonymous=False)
+    rospy.init_node('nav', anonymous=False)
     # Make an object of GoToPose
     navigator = GoToPose()
     
