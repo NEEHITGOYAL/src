23c77ca5ce3522046cac7212f5c1b1fe5dcfec28
neehit goyal <goyalneehit@gmail.com>
2021-02-04 21:16:41 +0530

    Task5 updates (#10)
    * minor changes
    
    * .
    
    * added session
    
    * added 2 objecys
    
    * added objects session file

--- ecto/doc/kitchen/doc_config.py
+++ ecto/doc/kitchen/doc_config.py
@@ -1,15 +1,15 @@
 version=''
-commithash='5b118836238efb9fedc9a951cd02736983b37917'
+commithash='69d821f5c93491872ef35b4fadbb250f63a8f005'
 gittag_short=''
-gittag_long='5b11883-dirty'
-git_lastmod='Mon, 25 Jan 2021 12:57:57 +0530'
+gittag_long='69d821f'
+git_lastmod='Fri, 29 Jan 2021 22:32:45 +0530'
 github_url='https://github.com/plasmodic/ecto'
 
 breathe_default_project = 'ecto'
-breathe_projects = dict(ecto='/home/narayan/catkin_ws/build/ecto/doc/../api/xml')
+breathe_projects = dict(ecto='/home/neehit/catkin_ws/build/ecto/doc/../api/xml')
 
 # for debug: this is only if you build everything locally
-#ecto_module_url_root = '/home/narayan/catkin_ws/build/ecto/doc/../../doc/html/'
+#ecto_module_url_root = '/home/neehit/catkin_ws/build/ecto/doc/../../doc/html/'
 # for release
 ecto_module_url_root = 'http://plasmodic.github.com/'
 
