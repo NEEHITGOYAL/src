089c38e86f6d00e59c72b6d9683765783821a598
Pratham Goyal <w3.goyal@gmail.com>
2021-03-03 12:47:06 +0530

    colour changed

--- sahayak_bot/ebot_task5_ur5/scripts/sb#1223_task6_original.py
+++ sahayak_bot/ebot_task5_ur5/scripts/sb#1223_task6_original.py
@@ -11,6 +11,7 @@ from trajectory_msgs.msg import *
 from visualization_msgs.msg import *
 from geometry_msgs.msg import Pose, Point, Quaternion
 from tf import TransformListener
+from colorama import Fore, Back, Style
 
 #declaring global variables
 global coke_target,battery_target,fgpa_target,glue_target,arm_group,hand_group
@@ -60,8 +61,6 @@ def baseArmPlanner(object_position,object_orientation):
         arm_group.set_goal_tolerance(0.01)
         # Check if planning was successfull              
         plan4 = arm_group.go()
-        # print the cordinates of object
-        #print("x = {},y = {},z = {},plan = {}".format(object_position[0], object_position[1], object_position[2], plan4))
 
 def armPlanner2(object_position):
     baseArmPlanner(object_position, [-0.033, 0.206, 0.940, -0.269])
@@ -69,63 +68,55 @@ def armPlanner2(object_position):
 def armPlanner(object_position):
     baseArmPlanner(object_position, [0.0, 0.0, -0.972, 0.234])
 
-def handle_result(result,position):
-    # If bot reached destination then print the cordinates of bot 
-    if result:
-        rospy.loginfo("I'm here now... (%s, %s) pose", position['x'], position['y'])
-    # If bot did not reach destination then print failure message 
-    else:
-        rospy.loginfo("The base failed to reach the desired pose")
-
 def getObjCordinates(obj):
     t=TransformListener()
+    # Detect object with target_frame obj
     t.waitForTransform("/ebot_base", obj, rospy.Time(), rospy.Duration(4.0))
+    # Find Coordinate of the object
     (objCordinates,rotation1) = t.lookupTransform("/ebot_base", obj, rospy.Time())
-    # Kill object detection nodes after successfull detection
-    # os.system("pkill gnome-terminal")
     return objCordinates
 
 def batteryArm(battery):
-     # Move arm in front of coke
+     # Move arm in front of battery
     armPlanner([battery[0] - 0.005, battery[1] - 0.40, battery[2] + 0.09])
     
-    # Move arm to grab coke
+    # Move arm to grab battery
     armPlanner([battery[0] - 0.005, battery[1] - 0.184, battery[2] + 0.09])
         
-    # Move gripper to close_coke pose
+    # Move gripper to close_battery pose
     gripperPose("close_battery") 
-    print("Battery Picked")
+    myPrint("Battery Picked")
     os.system('rosservice call /move_base/clear_costmaps "{}"')
     # Move arm back
     armPlanner([battery[0] - 0.005, battery[1] - 0.40, battery[2] + 0.2])
     armPose("travel2")
 
 def fpgaArm(fgpa):
-    # Move arm in front of glue
+    # Move arm in front of fgpa
     armPlanner2([fgpa[0] - 0.15, fgpa[1] - 0.3, fgpa[2] + 0.2])
     
-    # Move arm to grab glue
+    # Move arm to grab fgpa
     armPlanner2([fgpa[0] - 0.13, fgpa[1] - 0.18, fgpa[2] + 0.15])
 
-    # Move gripper to close_glue pose
+    # Move gripper to close_fgpa pose
     gripperPose("close_fgpa")
     os.system('rosservice call /move_base/clear_costmaps "{}"')
-    print("FPGA board picked")
+    myPrint("FPGA board picked")
 
 def glueArm(glue):
     # Move arm in front of glue
-        armPlanner([glue[0] - 0.008, glue[1] - 0.4, glue[2] + 0.1])
+    armPlanner([glue[0] - 0.008, glue[1] - 0.4, glue[2] + 0.1])
 
-        # Move arm to grab glue
-        armPlanner([glue[0] - 0.008, glue[1] - 0.195, glue[2] + 0.1])
+    # Move arm to grab glue
+    armPlanner([glue[0] - 0.008, glue[1] - 0.195, glue[2] + 0.1])
 
-        # Move gripper to close_glue pose
-        gripperPose("close_glue")
-        print("Glue Picked")
+    # Move gripper to close_glue pose
+    gripperPose("close_glue")
+    myPrint("Glue Picked")
 
-        os.system('rosservice call /move_base/clear_costmaps "{}"')
-        # Move arm back
-        armPlanner([glue[0] - 0.008, glue[1] - 0.195, glue[2] + 0.2])
+    os.system('rosservice call /move_base/clear_costmaps "{}"')
+    # Move arm back
+    armPlanner([glue[0] - 0.008, glue[1] - 0.195, glue[2] + 0.2])
 
 def cokeArm(coke):
     # Move arm in front of coke
@@ -136,7 +127,7 @@ def cokeArm(coke):
         
     # Move gripper to close_coke pose
     gripperPose("close_coke") 
-    print("Coke Picked")
+    myPrint("Coke Picked")
     os.system('rosservice call /move_base/clear_costmaps "{}"')
     # Move arm back
     armPlanner([coke[0], coke[1] - 0.4, coke[2] + 0.2])
@@ -182,13 +173,18 @@ class GoToPose():
             self.move_base.cancel_goal()
         rospy.sleep(1)
 
+# function to print with different colour
+def myPrint(string):
+    print(Fore.GREEN + Back.WHITE + string + Style.RESET_ALL)
+
 def bot_driver():
     # Initializes the ROS node for the process.
     rospy.init_node('nav', anonymous=False)
     # Make an object of GoToPose
     navigator = GoToPose()
+    # Make an object of TransformListener
     t = TransformListener() 
-    print("Started Run!")
+    myPrint("Started Run!")
     moveit_commander.roscpp_initialize(sys.argv)
     robot = moveit_commander.RobotCommander()
     hand_group = moveit_commander.MoveGroupCommander("grip_planning_group")
@@ -201,7 +197,6 @@ def bot_driver():
     frequency = 200
     # Bot reached destination or not
     result = navigator.goto(position, quaternion, frequency)
-
 
     os.system('rosservice call /move_base/clear_costmaps "{}"')
 
@@ -254,7 +249,7 @@ def bot_driver():
 
     armPose("drop_left")
     gripperPose("open")
-    print("Battery Dropped in DropBox3")
+    myPrint("Battery Dropped in DropBox3")
 
     armPose("travel2")   
     
@@ -341,7 +336,7 @@ def bot_driver():
 
     armPose("drop_left")
     gripperPose("open")
-    print("Coke dropped in Dropbox1")     
+    myPrint("Coke dropped in Dropbox1")     
     
     armPose("travel2")
     # Cordinates of Waypoint 8
@@ -351,7 +346,7 @@ def bot_driver():
     # Bot reached destination or not
     result = navigator.goto(position, quaternion, frequency)
 
-    print("Mission Accomplished!");
+    myPrint("Mission Accomplished!");
 
 
 # Python Main
